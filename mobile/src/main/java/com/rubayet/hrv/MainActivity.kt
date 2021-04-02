e5e85af4b268e97a10c8cb353ce4f1c308c483f0
Rubayet <rubayet.evan@gmail.com>
2021-04-02 17:24:29 +0600

    Fragment activity added

--- mobile/src/main/java/com/rubayet/hrv/MainActivity.kt
+++ mobile/src/main/java/com/rubayet/hrv/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.rubayet.hrv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
